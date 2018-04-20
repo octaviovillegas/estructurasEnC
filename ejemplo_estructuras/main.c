87fe2da385e65f68442b3bfcfc502a5896b562b7
mercedesguerrero <m.mercedesguerrero@gmail.com>
2018-04-20 17:31:17 -0300

    primera estructura

---
+++ ejemplo_estructuras/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int legajo;
+    int notaUno;
+    int notaDos;
+    float promedio;
+    char nombre[50];
+    int estado;
+
+}eAlumnos;
+
+int main()
+{
+
+
+
+    return 0;
+}
