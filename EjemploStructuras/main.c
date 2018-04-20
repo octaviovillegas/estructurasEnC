e9dc451cdcd6a9df9e317f1e5ea7dc97bf0e4481
pepe <sapo@pepe.com>
2018-04-20 17:31:22 -0300

    primerStructura

---
+++ EjemploStructuras/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    int legajo;
+    int notaUno;
+    int notaDos;
+    float promedio;
+    char nombre[50];
+}
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
